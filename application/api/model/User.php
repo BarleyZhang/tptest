5e5c69cc458f2819ee4684caccab265ab5653876
Barley <memeguigui@qq.com>
2019-01-03 16:32:03 +0800

    新增用户模型

--- application/api/model/User.php
+++ application/api/model/User.php
@@ -1,9 +1,18 @@
 <?php
+
 namespace app\api\model;
 
 use think\Model;
 
+/**
+ * 用户表自定义模型
+ * Class User
+ * @package app\api\model
+ */
 class User extends Model
 {
-
+    //自动写入时间戳
+    protected $autoWriteTimestamp = true;
+    //数据写入时,状态字段默认为1
+    protected $insert = ['status' => 1];
 }
