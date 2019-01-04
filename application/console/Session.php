b82d599b54a6fd5c738f68b6afd00e138dfe2850
Barley <memeguigui@qq.com>
2019-01-04 10:46:12 +0800

    临时测试修改命令行

--- application/console/Session.php
+++ application/console/Session.php
@@ -1,30 +1,39 @@
 <?php
+
+namespace app\console;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Argument;
+use think\console\input\Option;
+use think\console\Output;
+
 /**
  * 会话控制
  */
 class Session extends Command
 {
-	
-	//指令配置
-	protected function configure()
-	{
-		//设置命令名,额外选项和描述信息
-		$this->setName('session')
-			->addOption('clear' , 'd' , Option::VALUE_NONE , 'clear all session' , null)
-			->setDescription('Clear Session file');
-	}
 
-	//指令操作
-	protected function execute(Input $input, Output $output)
-	{
-		//获取指令想选名称
-		$path = $input->getOption('clear');
-		if ($path) {
-			//执行清楚会话信息操作
-			unset($_SESSION);
-			$output->writeln('Clear Session Successed');
-		}else{
-			$output->writeln('Clear Nothing');
-		}
-	}
+    //指令配置
+    protected function configure()
+    {
+        //设置命令名,额外选项和描述信息
+        $this->setName('session')
+            ->addOption('clear', 'd', Option::VALUE_NONE, 'clear all session', null)
+            ->setDescription('Clear Session file');
+    }
+
+    //指令操作
+    protected function execute(Input $input, Output $output)
+    {
+        //获取指令想选名称
+        $path = $input->getOption('clear');
+        if ($path) {
+            //执行清楚会话信息操作
+            unset($_SESSION);
+            $output->writeln('Clear Session Successed');
+        } else {
+            $output->writeln('Clear Nothing');
+        }
+    }
 }
